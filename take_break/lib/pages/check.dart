4d899606251deb0041e9c4fb502095470fd51253
cyin100 <conner.yin@gmail.com>
2022-11-13 05:32:19 -0800

    Checklist + some rating changes

--- take_break/lib/pages/check.dart
+++ take_break/lib/pages/check.dart
@@ -1,5 +1,24 @@
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+
+var categoriesList = [
+  "amusement_park",
+  "aquarium",
+  "art_gallery",
+  "bowling_alley",
+  "book_store",
+  "casino",
+  "night_club",
+  "museum",
+  "park",
+  "spa",
+  "tourist_attraction",
+  "zoo",
+  "cafe",
+  "restaurant",
+  "bar"
+];
 
 class MyWidget extends StatefulWidget {
   const MyWidget({super.key});
@@ -11,6 +30,24 @@ class MyWidget extends StatefulWidget {
 class _MyWidgetState extends State<MyWidget> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Card(
+        child: InkWell(
+          splashColor: Colors.green.withAlpha(30),
+          onTap: () {
+            debugPrint('Card tapped.');
+          },
+          child: const SizedBox(
+            width: 300,
+            height: 100,
+            child: Text("TEST",
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black,
+                )),
+          ),
+        ),
+      ),
+    );
   }
 }
