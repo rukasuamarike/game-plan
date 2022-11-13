017781ffb22da6c908af5cfee5e3c9c334f27738
rukasuamarike <32651190+rukasuamarike@users.noreply.github.com>
2022-11-12 18:20:49 -0800

    Create form.dart

---
+++ take_break/lib/pages/form.dart
@@ -0,0 +1,63 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class InputFormPage extends StatefulWidget {
+  const InputFormPage({Key? key}) : super(key: key);
+
+  @override
+  _InputFormPageState createState() => _InputFormPageState();
+}
+
+class _InputFormPageState extends State<InputFormPage> {
+  TextEditingController dateController = TextEditingController();
+
+  @override
+  void initState() {
+    dateController.text = ""; //set the initial value of text field
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text("Datepicker")),
+        body: Container(
+            padding: const EdgeInsets.all(15),
+            height: 150,
+            child: Center(
+                child: TextField(
+              controller: dateController, //editing controller of this TextField
+              decoration: const InputDecoration(
+                  icon: Icon(Icons.calendar_today), //icon of text field
+                  labelText: "Enter Date" //label text of field
+                  ),
+              readOnly: true, // when true user cannot edit text
+              onTap: () async {
+                DateTime? pickedDate = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(), //get today's date
+                    firstDate: DateTime
+                        .now(), //DateTime.now() - not to allow to choose before today.
+                    lastDate: DateTime(2101));
+
+                if (pickedDate != null) {
+                  print(
+                      pickedDate); //get the picked date in the format => 2022-07-04 00:00:00.000
+                  String formattedDate = pickedDate
+                      .toString(); // format date in required form here we use yyyy-MM-dd that means time is removed
+                  print(
+                      formattedDate); //formatted date output using intl package =>  2022-07-04
+                  //You can format date as per your need
+
+                  setState(() {
+                    dateController.text =
+                        formattedDate; //set foratted date to TextField value.
+                  });
+                } else {
+                  print("Date is not selected");
+                }
+              },
+            ))));
+  }
+}
